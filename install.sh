fa41f5d1d750bcac04d821c23ac340c87698bf26
Eric Martindale <eric@ericmartindale.com>
2015-09-22 20:04:40 -0700

    Add markdown syntax highlighting.

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ mkdir ~/tmp
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 # install packages
 git clone git://github.com/digitaltoad/vim-jade.git ~/.vim/bundle/vim-jade
+git clone git://github.com/tpope/vim-markdown.git ~/.vim/bundle/vim-markdown
